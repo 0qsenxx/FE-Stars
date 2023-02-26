29757dca964377fdc0fa1194ca2e63fad407c83c
Yakov29 <108018683+Yakov29@users.noreply.github.com>
2023-02-26 19:08:55 +0000

    Deploying to gh-pages from @ AntipyukSemen/FE-Stars@6dedb7cd4790396c68d80fc687d4f00cd7b9b6f3 🚀

---
+++ index.f7097d45.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-open-modal]"),closeModalBtn:document.querySelector("[data-close-modal]"),backdrop:document.querySelector("[data-backdrop]")};function o(){e.backdrop.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o),e.backdrop.addEventListener("click",(function(){console.log("Це клік в бекдроп")}))})();
+//# sourceMappingURL=index.f7097d45.js.map
